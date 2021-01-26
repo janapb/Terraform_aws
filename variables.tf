fa6d33db0db02c8a65d72da115bee656fb96f8de
janapb <52954974+janapb@users.noreply.github.com>
2021-01-26 11:49:09 +0800

    Updated with Azure variables

--- variables.tf
+++ variables.tf
@@ -1,2 +1,13 @@
-variable "region"{}
-variable "type" {}
+variable "subscription_id" {
+  description = "Enter Subscription ID for provisioning resources in Azure"
+}
+variable "client_id" {
+  description = "Enter Client ID for provisioning resources in Azure"
+}
+
+variable "client_secret" {
+  description = "Enter Client secret for provisioning resources in Azure"
+}
+variable "tenant_id" {
+  description = "Enter Tenant ID / Directory ID  for provisioning resources in Azure"
+}
